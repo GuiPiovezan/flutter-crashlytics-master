981733e916ca4f77d1b5ade00b1291edad4b45ea
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-07-07 21:05:55 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_crashlytics_master/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_crashlytics_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:alura_crashlytics/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(71, 161, 56, 1),
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Color.fromRGBO(71, 161, 56, 1),
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
